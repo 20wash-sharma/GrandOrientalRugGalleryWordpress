c897afff17a6a53748d52281fa6cafc5fcff12ba
Roshan Banstola <roshanb54@gmail.com>
2019-11-30 17:37:55 +0545

    Woocommerce added in theme folder

--- wp-content/themes/gorg/inc/woocommerce.php
+++ wp-content/themes/gorg/inc/woocommerce.php
@@ -268,3 +268,15 @@ if ( ! function_exists( 'gorg_woocommerce_header_cart' ) ) {
 		<?php
 	}
 }
+/*Remove add to cart button & prices*/
+remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart');
+remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price', 10 );
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
+add_filter( 'woocommerce_variable_sale_price_html', 'roshan_remove_prices', 10, 2 );
+add_filter( 'woocommerce_variable_price_html', 'roshan_remove_prices', 10, 2 );
+add_filter( 'woocommerce_get_price_html', 'roshan_remove_prices', 10, 2 );
+ 
+function roshan_remove_prices( $price, $product ) {
+if ( ! is_admin() ) $price = '';
+return $price;
+}

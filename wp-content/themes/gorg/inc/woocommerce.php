64fa5396848ea3704267196d45dd0eed1dedf312
Roshan Banstola <roshanb54@gmail.com>
2019-12-03 22:32:33 +0545

    Product title show

--- wp-content/themes/gorg/inc/woocommerce.php
+++ wp-content/themes/gorg/inc/woocommerce.php
@@ -171,7 +171,6 @@ remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wra
 remove_action( 'woocommerce_before_main_content','woocommerce_breadcrumb', 20, 0);
 remove_action( 'woocommerce_sidebar', 'woocommerce_get_sidebar', 10 );
 remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_rating', 5 );
-remove_action( 'woocommerce_shop_loop_item_title', 'woocommerce_template_loop_product_title', 10 );
 remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price', 10 );
 remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_show_product_loop_sale_flash', 10 );
 

c49d26c40faf685b09f506197f8a0d61efb240d4
Roshan Banstola <roshanb54@gmail.com>
2019-12-14 20:34:27 +0545

    Set the zoom script for default gutenberg gallery

--- wp-content/themes/gorg/assets/js/scripts.js
+++ wp-content/themes/gorg/assets/js/scripts.js
@@ -467,6 +467,23 @@
 		preloader: true,
 		mainClass: 'mfp-zoom'
 	});
+	//gutenburg default gallery popup
+	$('.wp-block-gallery').each(function () { // the containers for all your galleries
+		$(this).magnificPopup({
+			delegate: 'a', // the selector for gallery item
+			type: 'image',
+			gallery: {
+				enabled: true
+			},
+			zoom: {
+				enabled: true,
+				duration: 300, // don't foget to change the duration also in CSS
+				opener: function (element) {
+					return element.find('img');
+				}
+			}
+		});
+	});
 
 	$('.catalog_img').each(function () { // the containers for all your galleries
 		$(this).magnificPopup({

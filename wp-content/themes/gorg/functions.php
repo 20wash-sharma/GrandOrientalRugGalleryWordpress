f8850eac2c1dd76a17cedafcdc20cd7c2d9bccb6
Roshan Banstola <roshanb54@gmail.com>
2019-12-15 22:18:54 +0545

    set max upload size

--- wp-content/themes/gorg/functions.php
+++ wp-content/themes/gorg/functions.php
@@ -6,6 +6,9 @@
  *
  * @package GORG
  */
+@ini_set( 'upload_max_size' , '64M' );
+@ini_set( 'post_max_size', '64M');
+@ini_set( 'max_execution_time', '300' );
 
 if ( ! function_exists( 'gorg_setup' ) ) :
 	/**

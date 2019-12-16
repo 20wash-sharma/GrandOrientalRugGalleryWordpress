ce5aacdc3a054ba74b729e04d676a35212b8582b
Roshan Banstola <roshanb54@gmail.com>
2019-12-16 19:12:45 +0545

    misc changed on script

--- wp-content/themes/gorg/assets/js/scripts.js
+++ wp-content/themes/gorg/assets/js/scripts.js
@@ -249,18 +249,14 @@
 					0: {
 						items: 1,
 						touchDrag: true,
-						autoplay: false,
 					},
 					380: {
 						items: 1,
 						touchDrag: true,
-						autoplay: false,
-						nav: false,
 					},
 					576: {
 						items: 2,
 						touchDrag: true,
-						autoplay: false,
 					},
 					1000: {
 						items: 3,
